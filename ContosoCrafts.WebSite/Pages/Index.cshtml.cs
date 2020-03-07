5225f5456b9b2e0d88a8e6dcd605660651ece8ea
larsnorberg <larsnorberg@hotmail.com>
2020-03-07 11:39:56 +0100

    initial

--- ContosoCrafts.WebSite/Pages/Index.cshtml.cs
+++ ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,8 +12,11 @@ namespace ContosoCrafts.WebSite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> 
+            logger, 
+            JsonFileProductService productService)
         {
             _logger = logger;
         }
